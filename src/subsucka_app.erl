7913926544e95afeebe487259deac280f34512b2
Scott Chacon <schacon@gmail.com>
2008-08-21 09:52:08 -0700

    simplified the api a bit, removed boilerplate comments, added stop() function, added interface routine for import call

--- src/subsucka_app.erl
+++ src/subsucka_app.erl
@@ -3,7 +3,7 @@
 -export([start/2, stop/1]).
 
 start(_Type, _StartArgs) ->
-  subsucka_server:start_link().
+  subsucka_server:start().
   
 stop(_State) ->
   subsucka_server:stop().
--- src/subsucka_server.erl
+++ src/subsucka_server.erl
@@ -3,88 +3,29 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([start/0]).
 
 %% gen_server callbacks
-% -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-%         terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
 -compile([export_all]).
 
--define(SERVER, subsuka_server).
+start() -> gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+stop()  -> gen_server:call(?MODULE, stop).
 
-%%====================================================================
-%% API
-%%====================================================================
-%%--------------------------------------------------------------------
-%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
-%% Description: Starts the server
-%%--------------------------------------------------------------------
-start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+import_uri(Uri) -> gen_server:call(?MODULE, {import, Uri}).
 
-%%====================================================================
-%% gen_server callbacks
-%%====================================================================
 
-%%--------------------------------------------------------------------
-%% Function: init(Args) -> {ok, State} |
-%%                         {ok, State, Timeout} |
-%%                         ignore               |
-%%                         {stop, Reason}
-%% Description: Initiates the server
-%%--------------------------------------------------------------------
-init([]) ->
-  {ok, []}.
+init([]) -> {ok, []}.
 
-%%--------------------------------------------------------------------
-%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
-%%                                      {reply, Reply, State, Timeout} |
-%%                                      {noreply, State} |
-%%                                      {noreply, State, Timeout} |
-%%                                      {stop, Reason, Reply, State} |
-%%                                      {stop, Reason, State}
-%% Description: Handling call messages
-%%--------------------------------------------------------------------
 handle_call({import, Uri}, _From, State) ->
   Reply = ok,
   io:format("~p~n", [Uri]),
-  {reply, Reply, State}.
-
-%%--------------------------------------------------------------------
-%% Function: handle_cast(Msg, State) -> {noreply, State} |
-%%                                      {noreply, State, Timeout} |
-%%                                      {stop, Reason, State}
-%% Description: Handling cast messages
-%%--------------------------------------------------------------------
-handle_cast(_Msg, State) ->
-  {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% Function: handle_info(Info, State) -> {noreply, State} |
-%%                                       {noreply, State, Timeout} |
-%%                                       {stop, Reason, State}
-%% Description: Handling all non call/cast messages
-%%--------------------------------------------------------------------
-handle_info(_Info, State) ->
-  {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% Function: terminate(Reason, State) -> void()
-%% Description: This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any necessary
-%% cleaning up. When it returns, the gen_server terminates with Reason.
-%% The return value is ignored.
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
-  ok.
-
-%%--------------------------------------------------------------------
-%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% Description: Convert process state when code is changed
-%%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
-
-%%--------------------------------------------------------------------
-%%% Internal functions
-%%--------------------------------------------------------------------
+  {reply, Reply, State};
+handle_call(stop, _From, State) ->
+  {stop, normal, stopped, []}.
+	
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok. 
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
